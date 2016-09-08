9c6e391b2a03a7fd6960053f746c0039c91e7b09
matthew brozen <matthewbrozen@gmail.com>
2016-09-07 20:12:07 -0700

    enable cors

--- routes/reports.js
+++ routes/reports.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 var reportsController = require('../controllers/reports')
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.get('/', reportsController.getAll)
 router.get('/:id', reportsController.getOne);
 router.put('/:id', reportsController.changeOne);
